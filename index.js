36a4d4245639efaff47fc9be60e3fd4dd9b77004
Parham Alvani <parham.alvani@gmail.com>
2017-07-29 09:44:41 +0430

    Bamboo component initiation was added.

--- index.js
+++ index.js
@@ -22,3 +22,17 @@ const winston = require('winston')
 /* Configure CLI output on the default logger */
 winston.cli()
 winston.info(' * 18.20 at Sep 07 2016 7:20 IR721')
+
+/* Bamboo component initiation */
+const BambooComponent = require('@ibamboo/component')
+
+new BambooComponent({
+    mqttHost: process.env.BAMBOO_CONNECTIVITY_HOST,
+    mqttPort: process.env.BAMBOO_CONNECTIVITY_PORT,
+    name: 'trap',
+    subscribes: ['log']
+}).on('ready', () => {
+    winston.info(` * MQTT at ${process.env.BAMBOO_CONNECTIVITY_HOST}:${process.env.BAMBOO_CONNECTIVITY_PORT}`)
+}).on('log', (message) => {
+    winston.data(message)
+})
