3f676a17b802881988d4285040aa02aca54931e4
mahtabfarrokh <mahtab.farrokh@gmail.com>
2017-08-08 00:19:46 +0430

    mqtt bug fixed

--- mqtt.js
+++ mqtt.js
@@ -2,22 +2,15 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
 var mqtt = require('mqtt');
-var conn = mqtt.connect( 'mqtt:iot.ceit.aut.ac.ir:58904', function(err, client) {
-  if (err) throw err;
-  client.connect({
-    protocolId: 'MQIsdp',
-    protocolVersion: 3,
-    clientId: 'example',
-    keepalive: 30000
-  });
-  client.on('connect' , function(){
+var client = mqtt.connect( 'mqtt:iot.ceit.aut.ac.ir:58904')
+  
+client.on('connect' , function(){
     console.log("connected ! ")
     client.subscribe('presence')
-    lient.publish('presence', 'Hello mqtt')
+    client.publish('presence', 'Hello mqtt')
   })
   client.on('message', function(topic, message) {
-    console.log(message.toString(topic))
+    console.log(topic)
     io.emit('my message' , topic)
-    socket.emit('my message', $('#m').val());
-  });
-});
+    // socket.emit('my message', $('#m').val());
+  });  
