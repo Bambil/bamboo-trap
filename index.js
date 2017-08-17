ca4269b456880f8a61d9e75a40c50b34e18a1ab0
Parham Alvani <parham.alvani@gmail.com>
2017-08-17 18:32:45 +0430

    Better module entry point but not awesome.

--- index.js
+++ index.js
@@ -8,13 +8,7 @@
  * +===============================================
  */
 /* Configuration */
-if (!process.env.BAMBOO_CONNECTIVITY_HOST) {
-    process.env.BAMBOO_CONNECTIVITY_HOST = '127.0.0.1'
-}
-
-if (!process.env.BAMBOO_CONNECTIVITY_PORT) {
-    process.env.BAMBOO_CONNECTIVITY_PORT = 1883
-}
+const config = require('config')
 
 /* winston.js */
 const winston = require('winston')
@@ -23,16 +17,28 @@ const winston = require('winston')
 winston.cli()
 winston.info(' * 18.20 at Sep 07 2016 7:20 IR721')
 
+/* socket.io initiation */
+const app = require('http').createServer()
+const io = require('socket.io')(app);
+
+app.listen(config.http.port)
+
 /* Bamboo component initiation */
 const BambooComponent = require('@ibamboo/component')
 
 new BambooComponent({
-    mqttHost: process.env.BAMBOO_CONNECTIVITY_HOST,
-    mqttPort: process.env.BAMBOO_CONNECTIVITY_PORT,
+    mqttHost: config.connectivity.host,
+    mqttPort: config.connectivity.port,
     name: 'trap',
     subscribes: ['log']
 }).on('ready', () => {
-    winston.info(` * MQTT at ${process.env.BAMBOO_CONNECTIVITY_HOST}:${process.env.BAMBOO_CONNECTIVITY_PORT}`)
+    winston.info(` * MQTT at ${config.connectivity.host}:${config.connectivity.port}`)
 }).on('log', (message) => {
-    winston.data(message)
+  winston.data(message)
+  io.emit('raw', {
+    type: 'log',
+    data: {
+      'agent_id': `${message.tenant}/${message.name}`
+    }
+  })
 })
