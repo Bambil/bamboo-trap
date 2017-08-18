d99dbba22913a7706cb7030cd5e323f862f00e95
Parham Alvani <parham.alvani@gmail.com>
2017-08-18 21:45:45 +0430

    Improves Bamboo Trap class and its filename.

--- src/trap.js
+++ src/trap.js
@@ -4,14 +4,16 @@
  * |
  * | Creation Date: 29-07-2017
  * |
- * | File Name:     tarp.js
+ * | File Name:     trap.js
  * +===============================================
  */
 
-
 class BambooTrap {
   constructor (port) {
-    this.io = require('socket.io')(port)
+    const app = require('http').createServer()
+    this.io = require('socket.io')(app)
+
+    app.listen(port)
   }
 
   message (name, data) {
