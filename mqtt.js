158aa45bfe1beb15cfe55f34cfadf2a3bcfaf08f
mahtabfarrokh <mahtab.farrokh@gmail.com>
2017-09-04 12:21:00 +0430

    multi threading added

--- mqtt.js
+++ mqtt.js
@@ -1,18 +1,53 @@
 
-var server = require('http').createServer();
-var io = require('socket.io')(server);
+var app = require('express')();
+const cluster = require('cluster');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var mqtt = require('mqtt');
-var client = mqtt.connect( 'mqtt:iot.ceit.aut.ac.ir:58904')
-  
-client.on('connect' , function(){
-    console.log("connected ! ")
-    client.subscribe('presence')
-    client.publish('presence', 'Hello mqtt')
-    io.emit('my message' , "helllooooo")
+const numCPUs = require('os').cpus().length;
 
-  })
-  client.on('message', function(topic, message) {
-    console.log("here")
-    console.log(topic , message )
-    // socket.emit('my message', $('#m').val());
-  });  
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+
+    //connect to mqtt
+    var client = mqtt.connect( 'mqtt:iot.ceit.aut.ac.ir:58904')
+
+    //sending counter message
+    client.on('connect' , function(){
+        console.log("connected ! ")
+        client.subscribe('presence') 
+        count = 0
+        setInterval(() => {
+	    count = count + 1
+            client.publish('presence', 'Hello mqtt '+ count.toString());
+        }, 1000)
+    })
+
+    //send message to socket io
+    client.on('message', function(topic, message) {
+        console.log(topic , message.toString())
+        io.emit('my message' , message.toString())  
+    });
+
+    //html file
+    app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+        });
+    
+    //http port 
+    http.listen(3000, function(){
+        console.log('listening on *:3000');
+    });
+
+  console.log(`Worker ${process.pid} started`);
+}
