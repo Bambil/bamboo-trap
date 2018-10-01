34d670c66d35de0da13eed8dd5f40751a50618d8
Parham Alvani <parham.alvani@gmail.com>
2018-10-01 16:30:52 +0330

    Changes component with native mqtt connection.

--- index.js
+++ index.js
@@ -15,30 +15,21 @@ const vorpal = require('vorpal')()
 const chalk = require('chalk')
 
 vorpal.log(' * 18.20 at Sep 07 2016 7:20 IR721')
-vorpal.delimiter(`${chalk.green('Bamboo')} - ${chalk.rgb(255, 177, 79)('Trap')} > `).show()
+vorpal.delimiter(`${chalk.green('I1820')} - ${chalk.rgb(255, 177, 79)('Trap')} > `).show()
 
-/* Bamboo component initiation */
-const BambooComponent = require('@ibamboo/component')
-const BambooTrap = require('./src/trap')
-const bambooTrap = new BambooTrap(config.http.port)
+/* I1820 component initiation */
+const I1820Trap = require('./src/trap')
+const i1820Trap = new I1820Trap(config.http.port)
 
-new BambooComponent({
-  mqttHost: config.connectivity.host,
-  mqttPort: config.connectivity.port,
-  name: 'trap',
-  subscribes: ['log']
-}).on('ready', () => {
+
+/* MQTT connection events and handlers */
+const mqtt = require('mqtt')
+var client = mqtt.connect(`mqtt://${config.connectivity.host}:${config.connectivity.port}`)
+client.on('connect', () => {
   vorpal.log(` * MQTT at ${config.connectivity.host}:${config.connectivity.port}`)
-}).on('log', (message) => {
+  client.subscribe('i1820/projects/+/things/+/assets/+/state')
+})
+client.on('message', (topic, message) => {
   vorpal.log(message)
-  bambooTrap.message('raw', {
-    type: 'log',
-    data: {
-      'agent_id': `${message.tenant}/${message.name}`,
-      'device_id': `${message.data.id}`,
-      'timestamp': `${message.data.timestamp}`,
-      'type': `${message.data.type}`,
-      'states': `${message.data.state}`
-    }
-  })
+  i1820Trap.message('raw', message)
 })
